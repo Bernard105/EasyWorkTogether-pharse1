ddf38309bc519cff808c9ecb95b53dcf5335024d
Trần Mạnh Tài <101389458+Bernard105@users.noreply.github.com>
2026-04-01 22:30:29 +0700

    Delete TaskManagement.Application directory

--- TaskManagement.Application/DTOs/Profile/UpdateProfileRequest.cs
+++
@@ -1,9 +0,0 @@
-using TaskManagement.Application.DTOs.Profile;
-
-namespace TaskManagement.Application.Interfaces;
-
-public interface IProfileService
-{
-    Task<ProfileResponse> GetProfileAsync(int userId);
-    Task<ProfileResponse> UpdateProfileAsync(int userId, UpdateProfileRequest request, Stream? avatarStream, string? avatarFileName);
-}
